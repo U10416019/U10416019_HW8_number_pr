0a8720532faa2126552101a16e7c403d87df1c7b
WenChi <u10416019@go.utaipei.edu.tw>
2016-04-30 23:04:25 +0800

    Let the user choose the method to calculate
    I let the user enter the number to determine what kind of method the user want to calculate, if the user enter the number is not in the range then let let the user enter again, and I also let the user enter the number of scale, and it will be used if the number is indivisible, then determine whether the scale number which the user enter is greater than zero, if the number is not greater than zero then let the user enter again.  Finally call the Calculator method which is in the PrecisionNumber class.

--- TestPrecisionNumber.java
+++ TestPrecisionNumber.java
@@ -20,5 +20,29 @@ public class TestPrecisionNumber{
 		PrecisionNumber precisionNumber = new PrecisionNumber(number1, number2);
 		
 		
+		//let the user choose the method of calculate
+		System.out.print("Please enter the number which you want to calculate (1 for add, 2 for subtract, 3 for multiply, 4 for divide) :");
+		int calculate = input.nextInt();
+		//if the number is not in the range, then let the user enter the number again
+		while(calculate < 1 || calculate > 4){
+			System.out.println("The number you enter is not in the range.");
+			System.out.print("Please enter the number which you want to calculate (1 for add, 2 for subtract, 3 for multiply, 4 for divide) :");
+			calculate = input.nextInt();
+		}
+		
+		
+		//let the user enter the scale
+		System.out.print("Please enter the scale you want if the number is indivisible : ");
+		int scale = input.nextInt();
+		//if the number is not greater than zero, then let the user enter the number agagin
+		while(scale < 0){
+			System.out.println("The number you enter is not greater than zero.");
+			System.out.print("Please enter the scale you want if the number is indivisible : ");
+			scale = input.nextInt();
+		}
+		
+		
+		//call the method in the PrecisionNumber class which is a method to calculate the numbers
+		precisionNumber.Calculate(calculate, scale);
 	}
 }
